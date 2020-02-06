e6ab3b5a1e34dfad8afacb1bec25e465acea168a
Reuben Ogbonna <reubenogbonna@gmail.com>
2020-02-06 09:18:36 -0500

    added project files

---
+++ add_two_numbers.py
@@ -0,0 +1,7 @@
+class Node:
+    """
+    Build your linked list node class here.
+    """
+
+def add_two_numbers(num1: Node, num2: Node) -> Node:
+    pass
---
+++ test_add_two_numbers.py
@@ -0,0 +1,33 @@
+import pytest
+from add_two_numbers import *
+
+@pytest.mark.skip()
+def test_add_two_numbers():
+    l1 = Node(3)
+    l1.next = Node(2)
+    l1.next.next = Node(1)
+
+    l2 = Node(1)
+    l2.next = Node(1)
+    l2.next.next = Node(1)
+
+    sum_list = add_two_numbers(l1, l2)
+
+    assert sum_list.data == 4
+    assert sum_list.next.data == 3
+    assert sum_list.next.next.data == 2
+
+@pytest.mark.skip()
+def test_add_with_carry():
+    l1 = Node(7)
+    l1.next = Node(2)
+    l1.next.next = Node(1)
+
+    l2 = Node(4)
+    l2.next = Node(7)
+
+    sum_list = add_two_numbers(l1, l2)
+
+    assert sum_list.data == 1
+    assert sum_list.next.data == 0
+    assert sum_list.next.next.data == 7
